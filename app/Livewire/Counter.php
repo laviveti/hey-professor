2a426027a150fba1d6f479842f42ee1b9ab331d3
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-22 08:34:10 -0300

    my firsts components

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -7,15 +7,15 @@ use Livewire\Component;
 
 class Counter extends Component
 {
-  public int $counter = 0;
+    public int $counter = 0;
 
-  public function render(): View
-  {
-    return view('livewire.counter');
-  }
+    public function render(): View
+    {
+        return view('livewire.counter');
+    }
 
-  public function count(): void
-  {
-    $this->counter++;
-  }
+    public function count(): void
+    {
+        $this->counter++;
+    }
 }
