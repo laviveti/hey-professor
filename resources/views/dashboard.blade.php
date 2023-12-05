5fd7c77980a489ac73fead83a38be95fcf379505
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-12-05 13:41:17 -0300

    my firsts components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,11 @@
     </h2>
   </x-slot>
 
-  <div class="py-12">
+  <div class="space-y-5 py-12">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div
         class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
-
-
           <livewire:counter />
         </div>
       </div>
@@ -21,8 +19,6 @@
       <div
         class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
-
-
           <livewire:counter-inline />
         </div>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
+    Auth::loginUsingId(1);
+
     return view('welcome');
 });
 
@@ -28,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
