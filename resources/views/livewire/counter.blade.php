2d80c692afebba5f70ef361a900eb8ee49f1eba6
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-12-06 15:54:00 -0300

    data binding

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -4,6 +4,11 @@
     + 1
   </x-primary-button>
   <br><br>
+
+  <x-text-input wire:model.live="counter" />
+  <x-primary-button wire:click="refresh">Refresh</x-primary-button>
+
+  <br><br>
   <div>
     <p>Name: {{ $name }}</p>
     <p>Last Name: {{ $lastName }}</p>
